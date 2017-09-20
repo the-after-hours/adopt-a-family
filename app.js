c59285fa61b0e911cde82321c18077da38b42c0e
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-09-20 13:57:27 -0700

    fixed spacing and changed arrow functions

--- app.js
+++ app.js
@@ -2,50 +2,53 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.render('landing');
+app.get('/', (req, res) => {
+  res.render('landing');
 });
 
 // DASHBOARD ROUTE
-app.get('/dashboard', function(req, res) {
-    var sampleEjsText = 'DASHBOARD ROUTE';
-    res.render('dashboard', {
-        sampleEjsText: sampleEjsText // This is how you pass stuff to your EJS partial.
-    });
+app.get('/dashboard', (req, res) => {
+  var sampleEjsText = 'DASHBOARD ROUTE';
+  res.render('dashboard', {
+    sampleEjsText: sampleEjsText // This is how you pass stuff to your EJS partial.
+  });
 });
 
 // WISHLIST (SECURE) ROUTE
-app.get("/secure/wishlist", function(req, res) {
-
+app.get('/wishlist', (req, res) => {
+  var sampleEjsText = 'WISHLIST ROUTE';
+  res.render('wishlist', {
+    sampleEjsText: sampleEjsText
+  });
 });
 
 // ORGANIZER (SECURE) ROUTE
-app.get("/secure/org", function(req, res) {
-    res.render('org/list');
+app.get('/secure/org', (req, res) => {
+  res.render('org/list');
 });
 
 // SHOW ROUTE
 // this route will show the specific wishlist ":id" on a separate page
-app.get("/secure/wishlist/:id", function(req, res) {
+app.get('/secure/wishlist/:id', (req, res) => {
 
 });
 
 // CREATE ROUTE
 // lets you create/add a new wishlist item to wishlist "id"
-app.post("/secure/wishlist/:id", function(req, res) {
-    // look up wishlist using id
-    // create new entries
-    // connect new entry to wishlist
-    // redirect to wishlist show page
+app.post('/secure/wishlist/:id', (req, res) => {
+  // Look up wishlist using id
+  // create new entries
+  // connect new entry to wishlist
+  // redirect to wishlist show page
 });
 
-app.listen(3000, function() {
-    console.log('server running on port 3000');
-    console.log('connect to localhost:3000');
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+  console.log('connect to localhost:3000');
 });
