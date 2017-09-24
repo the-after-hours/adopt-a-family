6c06db0b8e703b9bb965be03aa81120859700fe4
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-09-24 13:23:15 -0700

    Updated routing structure to reload new page. incorporated react's Switch component so each new view is mathced with the corresponding route

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { Link, BrowserRouter as Router, Route } from  'react-router-dom';
-import Dashboard from './dashboard';
-import WishlistPublic from './wishlistPublic';
-import WishlistPrivate from './wishlistPrivate';
-import Pairing from './pairing';
-import Messaging from './messaging';
-import SecureOrg from './secureOrg';
+import Main from './main';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Router>
-          <ul>
-            <li>
-              <Link to="/dashboard">TEXT</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">TEXT</Link>
-            </li>
-            <li>
-            </li>
-            <li>
-            </li>
-            <li>
-            </li>
-            <li>
-            </li>
-          </ul>
-        </Router>
+        <Main />
       </div>
     );
   }
