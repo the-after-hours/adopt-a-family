8436ec98b296afce1152326161e57ca3ec63dec6
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-09-24 01:44:38 -0700

    Added placeholder components for each page. Will need to revisit routing to decide on multi-ppage or single-page applications.

---
+++ src/components/messaging.js
@@ -0,0 +1,15 @@
+// Messaging page to connect different parties.
+
+import React from 'react';
+
+class Messaging extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Messaging page</h1>
+      </div>
+    );
+  }
+}
+
+export default Messaging;
---
+++ src/components/wishlistPublic.js
@@ -0,0 +1,15 @@
+// Page to create/view wishlists
+
+import React from 'react';
+
+class WishlistPublic extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>WishlistPublic page</h1>
+      </div>
+    );
+  }
+}
+
+export default WishlistPublic;
