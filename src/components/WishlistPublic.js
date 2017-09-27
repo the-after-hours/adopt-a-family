f909c7117d59eedbced3cd773e2bad74baae3af2
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-09-27 00:05:26 -0700

    convert classes to stateless functions and set up route for about us and set up link on landing page

--- src/components/WishlistPublic.js
+++ src/components/WishlistPublic.js
@@ -2,14 +2,12 @@
 
 import React from 'react';
 
-class WishlistPublic extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>WishlistPublic page</h1>
-      </div>
-    );
-  }
-}
+const WishlistPublic = (props) => {
+  return (
+    <div>
+      <h1>WishlistPublic page</h1>
+    </div>
+  );
+};
 
 export default WishlistPublic;
