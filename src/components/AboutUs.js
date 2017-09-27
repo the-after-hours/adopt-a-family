329e5d4721a9f98e91cb2fbc59cfa6b8b3a6b9b3
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-09-27 00:05:26 -0700

    convert classes to stateless functions and set up route for about us and set up link on landing page

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+// Page to create/About Us
+
+import React from 'react';
+
+const AboutUs = (props) => {
+  return (
+    <div>
+      <h1>AboutUs page</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/components/SecureOrg.js
+++ src/components/SecureOrg.js
@@ -2,14 +2,12 @@
 
 import React from 'react';
 
-class SecureOrg extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>SecureOrg page</h1>
-      </div>
-    );
-  }
-}
+const SecureOrg = (props) => {
+  return (
+    <div>
+      <h1>SecureOrg page</h1>
+    </div>
+  );
+};
 
 export default SecureOrg;
