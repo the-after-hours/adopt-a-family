72bce346206835626192b20d029ee85be77da65e
Phuc Le <le.phuc.h@gmail.com>
2017-09-30 21:36:45 -0700

    Move CSS files to src/styles.  Build first material component to verify working theme.  Implement custom theme.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from  'react-router-dom';
 import App from './components/App';
+import './styles/index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const customTheme = getMuiTheme({
+  'palette': {
+    'primary1Color': '#006064',
+    'primary2Color': '#1a237e',
+    'accent1Color': '#bf360c',
+    'pickerHeaderColor': '#00838f'
+  }
+});
 
 ReactDOM.render(
   <Router>
-    <App />
+    <MuiThemeProvider muiTheme={customTheme}>
+      <App />
+    </MuiThemeProvider>
   </Router>,
   document.getElementById('root')
 );
