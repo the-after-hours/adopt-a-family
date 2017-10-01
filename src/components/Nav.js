df0b40a4169f0a73f206908de3f34b76b41748b5
Phuc Le <le.phuc.h@gmail.com>
2017-09-30 21:36:45 -0700

    Move CSS files to src/styles.  Build first material component to verify working theme.  Implement custom theme.

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Nav = (props) => {
+  return (
+    <div>
+      <AppBar
+        title="Adopt-a-Family"
+      />
+    </div>
+  )
+};
+
+export default Nav;
