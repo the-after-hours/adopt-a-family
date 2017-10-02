e346a233b7f56557a85be0ab1748980e72da99da
Phuc Le <le.phuc.h@gmail.com>
2017-10-02 00:31:26 -0700

    Move custom theme into its own folder.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import { BrowserRouter as Router } from  'react-router-dom';
 import App from './components/App';
 import './styles/index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
-const customTheme = getMuiTheme({
-  'palette': {
-    'primary1Color': '#006064',
-    'primary2Color': '#1a237e',
-    'accent1Color': '#bf360c',
-    'pickerHeaderColor': '#00838f'
-  }
-});
+import customTheme from '../theme';
 
 ReactDOM.render(
   <Router>
