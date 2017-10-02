6d80e9b2b65ff4d4b3813ab05324694e1532f31e
Phuc Le <le.phuc.h@gmail.com>
2017-10-02 00:31:26 -0700

    Move custom theme into its own folder.

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const customTheme = getMuiTheme({
+  'palette': {
+    'primary1Color': '#006064',
+    'primary2Color': '#1a237e',
+    'accent1Color': '#bf360c',
+    'pickerHeaderColor': '#00838f'
+  }
+});
+
+export default customTheme;
