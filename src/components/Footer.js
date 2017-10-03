8f2cad3965be00440d32556b2247d16b2a668a4e
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-03 00:52:18 -0700

    Update Footer.js structure to fix formatting. update tests and snapshots. Add links.js to house commonly used links. css updates.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import links from '../constants/links';
 
 const Footer = (props) => {
   return (
-    <div className="" id="footer">
-      <div className="social-icon-group">
-        <div className="block-center">
-          <a href="http://github.com/5-gwoap" className="social-icon">
-            <i className="fa fa-github fa-2x"></i>
-          </a>
-          <a href="#" className="social-icon">
-            <i className="fa fa-envelope fa-2x"></i>
-          </a>
+    <div id="footer">
+      <div className ="block-center">
+        <div className="social-icon-group">
+          <table>
+            <tr>
+              <td>
+                <a href={links.github} className="social-icon">
+                  <i className="fa fa-github fa-2x"></i>
+                </a>
+              </td>
+              <td>
+                <a href={links.email} className="social-icon">
+                  <i className="fa fa-envelope fa-2x"></i>
+                </a>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
       <div className="block-center">
         <div className="footer-text">
-          Code written under <a href="https://opensource.org/licenses/mit-license.html">MIT license</a>
+          Code written under <a href={links.license}>MIT license</a>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Footer;
---
+++ src/constants/links.js
@@ -0,0 +1,5 @@
+module.exports = {
+  github: 'https://github.com/5-gwoap',
+  email: 'mailto:5gwoap@gmail.com',
+  license: 'https://opensource.org/licenses/mit-license.html'
+};
