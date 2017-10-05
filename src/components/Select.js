22967e640f7c0e859085682f068bdc20786f028c
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-04 21:14:44 -0700

    Create Registration view, add form component, create reusable input and select tags. update mainjs routes.

---
+++ src/components/Select.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Select = (props) => {
+  return (
+      <select>
+        <option value="donator">Donator</option>
+        <option value="family">Family</option>
+        <option value="organizer">Organizer</option>
+      </select>
+    )
+}
+
+export default Select;
