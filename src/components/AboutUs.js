1eb308db211b7cea88a8e524605674b01a0d1037
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-04 19:47:16 -0700

    Add link to github on about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,11 +1,14 @@
 // Page to create/About Us
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import links from '../constants/links';
 
 const AboutUs = (props) => {
   return (
     <div>
       <h1>AboutUs page</h1>
+      Check us out on <a href={links.github}>Github</a>!
     </div>
   );
 };
