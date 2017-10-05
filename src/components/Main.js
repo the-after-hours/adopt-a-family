800cf7b2c35fa0fbd4666ac74b7d7bbe708224fe
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-04 21:14:44 -0700

    Create Registration view, add form component, create reusable input and select tags. update mainjs routes.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import WishlistPublic from './WishlistPublic';
-import WishlistPrivate from './WishlistPrivate';
+
 import AboutUs from './AboutUs';
-import Pairing from './Pairing';
-import Messaging from './Messaging';
-import SecureOrg from './SecureOrg';
-import Nav from './Nav';
+import Dashboard from './Dashboard';
 import Footer from './Footer';
+import Landing from './Landing';
+import Messaging from './Messaging';
+import Nav from './Nav';
+import Pairing from './Pairing';
+import Registration from './Registration';
+import SecureOrg from './SecureOrg';
+import WishlistPrivate from './WishlistPrivate';
+import WishlistPublic from './WishlistPublic';
 
 const Main = (props) => {
   return (
@@ -17,13 +19,14 @@ const Main = (props) => {
       <Nav />
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/wishlist" component={WishlistPublic} />
         <Route path="/aboutus" component={AboutUs} />
-        <Route path="/secure/wishlist" component={WishlistPrivate} /> {/* Secure */}
-        <Route path="/secure/pairing" component={Pairing} /> {/* Secure */}
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/secure/messaging" component={Messaging} /> {/* Secure */}
         <Route path="/secure/org" component={SecureOrg} /> {/* Secure */}
+        <Route path="/secure/pairing" component={Pairing} /> {/* Secure */}
+        <Route path="/secure/wishlist" component={WishlistPrivate} /> {/* Secure */}
+        <Route path="/registration" component={Registration} />
+        <Route path="/wishlist" component={WishlistPublic} />
       </Switch>
 
       <Footer />
---
+++ src/components/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from './Form';
+
+const Registration = (props) => {
+  return (
+      <div>
+        <Form />
+      </div>
+    )
+}
+
+export default Registration;
