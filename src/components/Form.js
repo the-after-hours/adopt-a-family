717dcb8d3d0f2493ea9d780ecb42200f6eb75f65
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-10-06 13:51:58 -0700

    add an extra new line between each input; add address, confirm email and password input; also add the submit button for the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,7 +14,7 @@ class Form extends React.Component {
             <Input
               placeholder="First Name"
               type="text"
-            /><br />
+            /><br /><br />
             <span className="contact-label">
               Middle Initial:
             </span>
@@ -22,18 +22,25 @@ class Form extends React.Component {
               maxlength="1"
               placeholder="Middle Initial"
               type="text"
-            /><br />
+            /><br /><br />
             <span className="contact-label">
               Last Name:
             </span>
             <Input
               placeholder="Last Name"
               type="text"
-            /><br />
+            /><br /><br />
+            <span className="contact-label">
+              Address
+            </span>
+            <Input
+              placeholder="Address"
+              type="text"
+            /><br /><br />
             <span className="contact-label">
               Account Type:
             </span>
-            <Select /><br />
+            <Select /><br /><br />
             <span className="contact-label">
               Email:
             </span>
@@ -41,7 +48,15 @@ class Form extends React.Component {
               placeholder="Email"
               type="email"
               pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-            /><br />
+            /><br /><br />
+            <span className="contact-label">
+              Confirm Email
+            </span>
+            <Input
+              placeholder="Confirm Email"
+              type="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+            /><br /><br />
             <span className="contact-label">
               Password:
             </span>
@@ -50,12 +65,21 @@ class Form extends React.Component {
               type="password"
               pattern=".{6,}"
               title="Must be at least 6 characters."
-            />
+            /><br /><br />
+            <span className="contact-label">
+              Confirm Password
+            </span>
+            <Input
+              placeholder='Confirm Password'
+              type="passwrd"
+              pattern=".{6,}"
+            /><br /><br />
+            <input className="contact-label" type="submit" value='Submit' />
           </form>
         </div>
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 export default Form;
