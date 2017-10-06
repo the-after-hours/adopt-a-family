5d2d81d4aeeb60ddaff7b8df4061e4f7b8a46cfe
Rain <rainrivas@gmail.com>
2017-10-06 02:03:26 -0700

    Adding placeholder content to pages found in nav bar. Updating snapshots so that tests will pass after the content changes. Also committing the auto-updated package-lock.

--- src/components/SecureOrg.js
+++ src/components/SecureOrg.js
@@ -5,7 +5,26 @@ import React from 'react';
 const SecureOrg = (props) => {
   return (
     <div>
-      <h1>SecureOrg page</h1>
+      <h1>Secure Org page</h1>
+      <h2>I&apos;m not sure how this will look really, but adding some paragraphs to get some conversation started</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   );
 };
--- src/components/WishlistPublic.js
+++ src/components/WishlistPublic.js
@@ -5,7 +5,33 @@ import React from 'react';
 const WishlistPublic = (props) => {
   return (
     <div>
-      <h1>WishlistPublic page</h1>
+      <h1>Wishlist Public Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <ul>
+        <li>
+          list element 1
+        </li>
+        <li>
+          list element 2
+        </li>
+        <li>
+          list element 3
+        </li>
+        <li>
+          list element 4
+        </li>
+      </ul>
     </div>
   );
 };
