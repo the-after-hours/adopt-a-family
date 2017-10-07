77e7f95f91e0e644a71d4c0d2377ea889edd89ea
Rain <rainrivas@gmail.com>
2017-10-06 22:57:13 -0700

    Correcting the spacing issues so that the linter test will pass and switching the other of the commands in npm test so that we call the linter before jest

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Select = (props) => {
   return (
-      <select>
-        <option value="donator">Donator</option>
-        <option value="family">Family</option>
-        <option value="organizer">Organizer</option>
-      </select>
-    )
-}
+    <select>
+      <option value="donator">Donator</option>
+      <option value="family">Family</option>
+      <option value="organizer">Organizer</option>
+    </select>
+  );
+};
 
 export default Select;
