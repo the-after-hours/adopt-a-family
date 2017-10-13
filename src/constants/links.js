6acfbf568384e617ab5185c74284d24c6fa5d5ed
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-13 00:36:57 -0700

    AAF-49 Add placeholder links

--- src/constants/links.js
+++ src/constants/links.js
@@ -2,5 +2,6 @@ module.exports = {
   github: 'https://github.com/5-gwoap/adopt-a-family',
   email: 'mailto:5gwoap@gmail.com',
   license: 'https://opensource.org/licenses/mit-license.html',
-  aboutus: 'https://github.com/5-gwoap'
+  aboutus: 'https://github.com/5-gwoap',
+  placeholder: 'http://www.theuselessweb.com/'
 };
