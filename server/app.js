86210989a27d05eba33d02decc3ea8c96de7b4c5
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2017-10-13 00:28:52 -0700

    Mofidying the path for express public folder

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./routes/api');
 // Eventually we will need to refer to the build folder for production
 // It would look like:
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 
 app.use('/', indexRoutes);
