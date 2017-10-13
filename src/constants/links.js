cf62702634d3d59aaa8a0fbb5f1f58fec14d90ba
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-13 00:24:47 -0700

    AAF-49 Update footer styling. Placeholder links in place. Pending final content

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,5 +1,6 @@
 module.exports = {
-  github: 'https://github.com/5-gwoap',
+  github: 'https://github.com/5-gwoap/adopt-a-family',
   email: 'mailto:5gwoap@gmail.com',
-  license: 'https://opensource.org/licenses/mit-license.html'
+  license: 'https://opensource.org/licenses/mit-license.html',
+  aboutus: 'https://github.com/5-gwoap'
 };
