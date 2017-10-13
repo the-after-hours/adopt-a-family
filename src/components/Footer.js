4aa1c1321a6b8482f0af6b3432ea28803297833c
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-13 01:18:17 -0700

    AAF-49 Remove unecessary attributes and source formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import links from '../constants/links';
 
-const d = new Date();
-const year = d.getFullYear();
-
 const Footer = (props) => {
   return (
-    <footer id="footer" className="footer">
+    <footer>
       <div className="footerText">
-        <div className="footerCopyright">&copy; {year} Adopt A Family</div>
+        <div className="footerCopyright">&copy; 2017 Adopt A Family</div>
         <div className="footerSpacer"> | </div>
         <div className="footerPrivacy">
           <a href={links.placeholder} target="_blank">Privacy Policy</a>
@@ -16,9 +13,7 @@ const Footer = (props) => {
         <div className="footerContact">
           <a href={links.placeholder} target="_blank">Contact Us</a>
         </div>
-        {/* <div id="license">Code written under <a href={links.license}>MIT license</a></div> */}
       </div>
-
       <div className="footerIcons">
         <a href={links.github} className="social-icon" target="_blank">
           <i className="fa fa-github fa-2x"></i>
@@ -29,7 +24,6 @@ const Footer = (props) => {
         <a href={links.aboutus} className="social-icon" target="_blank">
           <i className=" fa fa-user-circle fa-2x"></i>
         </a>
-
       </div>
     </footer>
   );
