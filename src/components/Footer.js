7ac6aed8777dac71ccae52fc8d6d647d66764fe3
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-13 00:24:47 -0700

    AAF-49 Update footer styling. Placeholder links in place. Pending final content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import links from '../constants/links';
 
+const d = new Date();
+const year = d.getFullYear();
+
 const Footer = (props) => {
   return (
-    <footer id="footer">
-      <div className ="block-center">
-        <div className="social-icon-group">
-          <table>
-            <tr>
-              <td>
-                <a href={links.github} className="social-icon">
-                  <i className="fa fa-github fa-2x"></i>
-                </a>
-              </td>
-              <td>
-                <a href={links.email} className="social-icon">
-                  <i className="fa fa-envelope fa-2x"></i>
-                </a>
-              </td>
-            </tr>
-          </table>
+    <footer id="footer" className="footer">
+      <div className="footerText">
+        <div className="footerCopyright">&copy; {year} Adopt A Family</div>
+        <div className="footerSpacer"> | </div>
+        <div className="footerPrivacy">
+          <a href="#">Privacy Policy</a>
         </div>
+        <div className="footerContact">
+          <a href="#">Contact Us</a>
+        </div>
+        {/* <div id="license">Code written under <a href={links.license}>MIT license</a></div> */}
       </div>
-      <div className="block-center">
-        <div className="footer-text">
-          Code written under <a href={links.license}>MIT license</a>
-        </div>
+
+      <div className="footerIcons">
+        <a href={links.github} className="social-icon">
+          <i className="fa fa-github fa-2x"></i>
+        </a>
+        <a href={links.email} className="social-icon">
+          <i className="fa fa-envelope fa-2x"></i>
+        </a>
+        <a href={links.aboutus} className="social-icon">
+          <i className=" fa fa-user-circle fa-2x"></i>
+        </a>
+
       </div>
     </footer>
   );
