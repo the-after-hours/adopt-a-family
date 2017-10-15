51c7235d7e77b06b036cefee048d437168161528
Phuc Le <le.phuc.h@gmail.com>
2017-10-14 23:49:38 -0700

    Add a tbody tag as direct child of table tag.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,20 @@ const Footer = (props) => {
       <div className ="block-center">
         <div className="social-icon-group">
           <table>
-            <tr>
-              <td>
-                <a href={links.github} className="social-icon">
-                  <i className="fa fa-github fa-2x"></i>
-                </a>
-              </td>
-              <td>
-                <a href={links.email} className="social-icon">
-                  <i className="fa fa-envelope fa-2x"></i>
-                </a>
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>
+                  <a href={links.github} className="social-icon">
+                    <i className="fa fa-github fa-2x"></i>
+                  </a>
+                </td>
+                <td>
+                  <a href={links.email} className="social-icon">
+                    <i className="fa fa-envelope fa-2x"></i>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
