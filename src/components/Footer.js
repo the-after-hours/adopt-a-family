051766103ca5adb96aa18c3207a316a9ec033554
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-17 22:09:33 -0700

    AAF-54 Fix security issue with noopener referrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,20 @@ const Footer = (props) => {
         <div className="footerCopyright">&copy; 2017 Adopt A Family</div>
         <div className="footerSpacer"> | </div>
         <div className="footerPrivacy">
-          <a href={links.placeholder} target="_blank">Privacy Policy</a>
+          <a href={links.placeholder} rel="noopener noreferrer" target="_blank">Privacy Policy</a>
         </div>
         <div className="footerContact">
-          <a href={links.placeholder} target="_blank">Contact Us</a>
+          <a href={links.placeholder} rel="noopener noreferrer" target="_blank">Contact Us</a>
         </div>
       </div>
       <div className="footerIcons">
-        <a href={links.github} className="social-icon" target="_blank">
+        <a href={links.github} className="social-icon" rel="noopener noreferrer" target="_blank">
           <i className="fa fa-github fa-2x"></i>
         </a>
-        <a href={links.email} className="social-icon" target="_blank">
+        <a href={links.email} className="social-icon" rel="noopener noreferrer" target="_blank">
           <i className="fa fa-envelope fa-2x"></i>
         </a>
-        <a href={links.aboutus} className="social-icon" target="_blank">
+        <a href={links.aboutus} className="social-icon" rel="noopener noreferrer" target="_blank">
           <i className=" fa fa-user-circle fa-2x"></i>
         </a>
       </div>
