21772bab3294f460716743131779989f43a6c0c8
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-21 09:23:05 -0700

    AAF-51 Update scripts to include node-sass-chokidar to preprocess scss files.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from  'react-router-dom';
 import App from './components/App';
-import './styles/index.css';
+import './build/styles/index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import customTheme from './theme';
 
