f3c2490b8afacbed9e87f34a4396e9ec15034e10
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-10-25 01:10:07 -0700

    AAF-44 Update to SASS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Nav from './Nav';
 
 const Header = (props) => {
   return (
     <header>
-      <div>
-        <Link to='/'>
-          Adopt A Family
-        </Link>
-      </div>
-      <div className="headerLinks">
-        <div className="headerMission">
-          Mission
-        </div>
-        <div className="headerFamily">
-          Our Families
-        </div>
-        <div className="headerAbout">
-          <Link to="/aboutus">
-            About us
-          </Link>
-        </div>
-        <div className="headerDonate">
-          Donate
-        </div>
-        <Link to="/registration">
-          <button className="buttonLogin">
-            ADOPT A FAMILY
-          </button>
-        </Link>
-      </div>
+      <Nav />
     </header>
   );
 };
