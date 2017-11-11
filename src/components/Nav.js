313a869d5198c9c88e22e21264419cd43ad5412d
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-11-11 01:25:31 -0800

    AAF-44 Remove material UI, update tests including MaterialUI theme.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,26 +6,28 @@ class Nav extends Component {
   render () {
     return (
       <div className="header-container">
-        <div className="header-text">
-          <Link to='/'>
+
+        <div className="col-3 header-group header-text">
+          <Link to="/">
             Adopt A Family
           </Link>
         </div>
-        <div className="header-links-group">
-          <Link className="header-links" to='/mission'>
+
+        <div className="col-8 header-group header-links-group">
+          <Link className="header-links" to="/mission">
             Mission
           </Link>
-          <Link className="header-links" to='/families'>
+          <Link className="header-links" to="/families">
             Our Families
           </Link>
           <Link className="header-links" to="/aboutus">
             About us
           </Link>
-          <Link className="header-links" to='/donate'>
+          <Link className="header-links" to="/donate">
             Donate
           </Link>
-          <Link to="/registration">
-            <button className='btn-registration'>
+          <Link className="header-links" to="/registration">
+            <button className="btn-registration">
               Adopt A Family
             </button>
           </Link>
