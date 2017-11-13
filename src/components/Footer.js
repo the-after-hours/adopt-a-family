c0252335f2adc935ee4f535381565f3b606819e4
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-11-12 21:19:23 -0800

    AAF-44 Update footer styling. Clean up redundant code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,24 @@ import links from '../constants/links';
 const Footer = (props) => {
   return (
     <footer>
-      <div className="footerText">
-        <div className="footerCopyright">&copy; 2017 Adopt A Family</div>
-        <div className="footerSpacer"> | </div>
-        <div className="footerPrivacy">
+      <div className="footer-text">
+        <div className="footer-item footer-copyright">&copy; 2017 Adopt A Family</div>
+        <div className="footer-item footer-spacer"> | </div>
+        <div className="footer-item footer-item">
           <a href={links.placeholder} rel="noopener noreferrer" target="_blank">Privacy Policy</a>
         </div>
-        <div className="footerContact">
+        <div className="footer-item">
           <a href={links.placeholder} rel="noopener noreferrer" target="_blank">Contact Us</a>
         </div>
       </div>
-      <div className="footerIcons">
-        <a href={links.github} className="social-icon" rel="noopener noreferrer" target="_blank">
+      <div className="footer-icons">
+        <a href={links.github} className="social-icon footer-item" rel="noopener noreferrer" target="_blank">
           <i className="fa fa-github fa-2x"></i>
         </a>
-        <a href={links.email} className="social-icon" rel="noopener noreferrer" target="_blank">
+        <a href={links.email} className="social-icon footer-item" rel="noopener noreferrer" target="_blank">
           <i className="fa fa-envelope fa-2x"></i>
         </a>
-        <a href={links.aboutus} className="social-icon" rel="noopener noreferrer" target="_blank">
+        <a href={links.aboutus} className="social-icon footer-item" rel="noopener noreferrer" target="_blank">
           <i className=" fa fa-user-circle fa-2x"></i>
         </a>
       </div>
