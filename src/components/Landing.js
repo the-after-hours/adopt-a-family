1e7f4198073079f3330cac5b6029ce64ba532777
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-11-14 23:43:22 -0800

    AAF-70 Chnage components to intrinsic returns

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,62 +3,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = (props) => {
-  return (
-    <div>
-      <h1>Landing page</h1>
-      <ul>
-        <li>
-          <Link to="/dashboard">Click me for dashboard</Link>
-        </li>
-        <li>
-          <Link to="/secure/pairing">Click me for secure/pairing</Link>
-        </li>
-        <li>
-          <Link to="/secure/messaging">Click me for messaging</Link>
-        </li>
-        <li>
-          <Link to="/wishlist">Click me for wishlistPublic</Link>
-        </li>
-        <li>
-          <Link to="/secure/wishlist">Click me for secure/wishlistPrivate</Link>
-        </li>
-        <li>
-          <Link to="/secure/org">Click me for secure/org</Link>
-        </li>
-        <li>
-          <Link to="/aboutus">Click me for about us</Link>
-        </li>
-        <li>
-          <Link to="/registration"><button>Register</button></Link>
-        </li>
-      </ul>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-      <p>
-        Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
-      </p>
-    </div>
-  );
-};
+const Landing = (props) => (
+  <div>
+    <h1>Landing page</h1>
+    <ul>
+      <li>
+        <Link to="/dashboard">Click me for dashboard</Link>
+      </li>
+      <li>
+        <Link to="/secure/pairing">Click me for secure/pairing</Link>
+      </li>
+      <li>
+        <Link to="/secure/messaging">Click me for messaging</Link>
+      </li>
+      <li>
+        <Link to="/wishlist">Click me for wishlistPublic</Link>
+      </li>
+      <li>
+        <Link to="/secure/wishlist">Click me for secure/wishlistPrivate</Link>
+      </li>
+      <li>
+        <Link to="/secure/org">Click me for secure/org</Link>
+      </li>
+      <li>
+        <Link to="/aboutus">Click me for about us</Link>
+      </li>
+      <li>
+        <Link to="/registration"><button>Register</button></Link>
+      </li>
+    </ul>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+    <p>
+      Aliquip laborum dolor cupidatat tempor ad laborum laboris minim nisi consequat cillum eiusmod eiusmod sit amet in exercitation aliquip in commodo aliqua tempor eu veniam do ut ut magna deserunt amet cillum magna veniam eiusmod in exercitation in magna quis anim quis incididunt voluptate mollit sit fugiat qui quis minim in adipisicing commodo nisi mollit fugiat cillum eu occaecat sed eiusmod consectetur quis officia ut laborum eu aliquip ut aliquip veniam incididunt incididunt in do deserunt voluptate fugiat eu et non nisi nostrud aliquip dolor deserunt dolor magna duis ad laborum sit excepteur ut deserunt do duis eu occaecat laborum anim et cillum laboris consequat nisi fugiat esse magna ut aliquip dolor culpa ut reprehenderit veniam ut ut culpa esse irure magna mollit cillum sit aliqua est ut non enim duis commodo proident amet pariatur laboris aliquip dolor et veniam qui anim ullamco ex exercitation qui elit occaecat in nisi cillum proident aliquip est occaecat ut elit.
+    </p>
+  </div>
+);
 
 export default Landing;
