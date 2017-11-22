7c7e008e2999806b24c6ffc79eee6b19e8189d7c
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2017-11-22 00:08:26 -0800

    Fixing Conflicts with development by pulling in dev code base

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Form from './Form';
 
-const Registration = (props) => {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
-};
+const Registration = (props) => (
+  <div>
+    <Form />
+  </div>
+);
 
 export default Registration;
