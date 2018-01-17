516456482fa2fd1d92933a8d0088436301fd2580
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-01-16 22:59:17 -0800

    Adding basic logic for /pairing route. Modifying Donor and Family schema to make references to each otehr for matching and adding default values to it. /pairing should find all donors and filter them based on budget then sort budgets in descending order.

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 
 // Possibly move these imports into sub files when each api route has it's own js file
 const mongoose = require('mongoose');
-// const Donor = require('../models/donor');
-// const Family = require('../models/family');
-
+const Donor = require('../models/donor');
+const Family = require('../models/family');
+// wishlish = Family.readWishlist();
 mongoose.connect('mongodb://localhost/aaf_local');
 
 app.use(bodyParser.json());
@@ -23,11 +23,60 @@ routes.get('/', (req, res) => {
   res.status(200).json({ message: 'You have hit the API.' });
 });
 
+// we are building Donors (those with goods) looking for families
 routes.get('/pairing', (req, res) => {
+  // an Orginization/Organizer calls this by passing FAMILY (wildcard invalid) and Budget
+  // it returns a list of donors with matching Family.wishlist.cost === Donor.budget +/- 10% [max $20, future implement] (from here on called 'budget')
+  // list is ordered with closest match (below target budget) then highest ones
+  // SPECIAL SORT RULE: First we will sort budget <= Family.wishlist.cost in DESCENDING then we will sort budget >= Family.wishlist.cost in ASCENDING order
+  // e.g. Family.wishlist.cost = 100 +/- 10%
+  // sorted list: 99, 98, 95, 90, 101, 102, 110
+  let maxBudget = req.params.budget*1.1;
+  let minBudget = req.params.budget*0.9;
+
+  Donor
+    .where('budget').gte(maxBudget).lte(minBudget)
+    .where('matchedFamily', 'unmatched')
+    .sort({ field: 'budget': -1 }) // take the donors found and sort budgets from highlest to lowest (ignores special rule)
+    .exec((err, donors) => {
+      // take the donors response and splice it at the value where it goes over/under budget
+      res.status(200).json({
+        message: 'Donors were found',
+        donors: donors
+      });
+    });
+  });
+
+routes.get('/pairing/balance', (req, res) => {
+  // This route is for finding the left over amount total (sum of all families money used)
+  // total in account (this can go up or down), tot
+  // total used, spent
+  // balance, tot-spent
+  res.status(200).json({ // comes from call to mongo
+    total: '50',
+    spent: '29',
+    balance: '21'
+  });
+});
+
+routes.get('/pairing/paired', (req, res) => {
   res.status(200).json({
-    message: 'hooray! pairing route!',
-    donor: { name: 'bob' },
-    family: { name: 'bill' }
+    org1: {
+      families: ['a','b','c'],
+      budget: { // comes from mongoDB, same as /paring/balance
+        total: '50',
+        spent: '29',
+        balance: '21'
+      }
+    },
+    org2: {
+      families: ['x','y','z'],
+      budget: { // comes from mongoDB, same as /paring/balance
+        total: '50',
+        spent: '29',
+        balance: '21'
+      }
+    }
   });
 });
 
