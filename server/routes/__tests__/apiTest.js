70f39fb3bc91768f1426b30a86d0941f275bac05
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-01-24 23:31:14 -0800

    WIP :: Setting up tests with base skeleton

---
+++ server/routes/__tests__/apiTest.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../server/app');
+
+describe('Test the root path', () => {
+  test('It should response the GET method', () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+});
+
