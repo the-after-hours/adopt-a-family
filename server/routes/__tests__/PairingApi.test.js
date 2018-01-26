f76056c1f2be69c7bd84eaec385c814fd5e507be
Rain <rainrivas@gmail.com>
2018-01-25 23:07:47 -0800

    Enhancing api tests to make proper assertions.

--- server/routes/__tests__/PairingApi.test.js
+++ server/routes/__tests__/PairingApi.test.js
@@ -1,13 +1,40 @@
 const request = require('supertest');
 const app = require('../../../server/app');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', () => {
+describe('Test /api/pairing', () => {
+  test('Sucessful queries return a 200', () => {
     return request(app)
-      .get('/')
+      .get('/api/')
       .then(response => {
         expect(response.statusCode).toBe(200);
       });
   });
+  test('Queries without budget param return 400', () => {
+    return request(app)
+      .get('/api/')
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+      });
+  });
+  test('Queries without wishlist param return 400', () => {
+    return request(app)
+      .get('/api/')
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+      });
+  });
+  test('If no results are found, appropriate message is shown', () => {
+    return request(app)
+      .get('/api/')
+      .then(response => {
+        expect(response.body.message).toBe('No donors were found');
+      });
+  });
+  test('If results are found print success message', () => {
+    return request(app)
+      .get('/api/')
+      .then(response => {
+        expect(response.body.message).toBe('Donors were found');
+      });
+  });
 });
-
