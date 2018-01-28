ae95fa1052529a14441121638b9d555aa86bb65a
Rain <rainrivas@gmail.com>
2018-01-28 00:14:25 -0800

    Adding logic to /api/pairing to handle missing budget params. Making tests more relevant to pairing route.

--- server/routes/__tests__/PairingApi.test.js
+++ server/routes/__tests__/PairingApi.test.js
@@ -1,40 +1,78 @@
 const request = require('supertest');
 const app = require('../../../server/app');
 
+const PAIRING_ENDPOINT = '/api/pairing';
 describe('Test /api/pairing', () => {
-  test('Sucessful queries return a 200', () => {
+  test('Should return 200 if valid budget param is passed', () => {
     return request(app)
-      .get('/api/')
+      .get('/api/pairing')
+      .query({ budget: 50000 })
       .then(response => {
         expect(response.statusCode).toBe(200);
       });
   });
-  test('Queries without budget param return 400', () => {
+  test('Should return 400 if budget param is not passed', () => {
     return request(app)
-      .get('/api/')
+      .get(PAIRING_ENDPOINT)
       .then(response => {
         expect(response.statusCode).toBe(400);
       });
   });
-  test('Queries without wishlist param return 400', () => {
+  test('Should return 400 if budget set to null', () => {
     return request(app)
-      .get('/api/')
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: 'null' })
       .then(response => {
         expect(response.statusCode).toBe(400);
       });
   });
-  test('If no results are found, appropriate message is shown', () => {
+  test('Should return 400 if budget set to undefined', () => {
     return request(app)
-      .get('/api/')
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: undefined })
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+      });
+  });
+  test('Should display error message if budget param is missing', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .then(response => {
+        expect(response.body.message).toBe('Missing Budget Param');
+      });
+  });
+  test.skip('Queries without wishlist param return 400 -- IS THIS VALID', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+      });
+  });
+  test('Should display appropriate message if no donors found', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: 400 })
       .then(response => {
         expect(response.body.message).toBe('No donors were found');
       });
   });
-  test('If results are found print success message', () => {
+  test('Should print success message if donors are found', () => {
     return request(app)
-      .get('/api/')
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: 50000 })
       .then(response => {
         expect(response.body.message).toBe('Donors were found');
+      });
+  });
+  // Skipping this until we enhance seed to include more donors
+  test.skip('Should sort donors in ascending budget order', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .query({ buddget: 50000 })
+      .then(response => {
+        expect(response.body.donors[0].budget).toBeGreaterThan(
+          response.body.donors[1].budget
+        );
       });
   });
 });
