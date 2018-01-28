278bf1f94cf1c46a2a6043a785c6000f374d3733
Rain <rainrivas@gmail.com>
2018-01-28 00:21:34 -0800

    Adding tests for making sure messages are only should when the appropriate number of donors are returned

--- server/routes/__tests__/PairingApi.test.js
+++ server/routes/__tests__/PairingApi.test.js
@@ -56,12 +56,28 @@ describe('Test /api/pairing', () => {
         expect(response.body.message).toBe('No donors were found');
       });
   });
+  test('Should only print no-donor message if donors found is 0', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: 400 })
+      .then(response => {
+        expect(response.body.donors).toHaveLength(0);
+      });
+  });
   test('Should print success message if donors are found', () => {
     return request(app)
       .get(PAIRING_ENDPOINT)
       .query({ budget: 50000 })
       .then(response => {
         expect(response.body.message).toBe('Donors were found');
+      });
+  });
+  test('Should only print donors-found message if donors found is >0', () => {
+    return request(app)
+      .get(PAIRING_ENDPOINT)
+      .query({ budget: 50000 })
+      .then(response => {
+        expect(response.body.donors).not.toHaveLength(0);
       });
   });
   // Skipping this until we enhance seed to include more donors
