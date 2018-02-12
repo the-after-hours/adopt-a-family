d53953d00dd64cd72614dd8e89105113d14d83cc
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-12 00:58:37 -0800

    AAF-95 Clean up addItem route. Add removeItem route. Update PUT to PATCH for addItem and removeItem route. Routes still WIP. No real error handling yet.

--- server/controllers/wishlistController.js
+++ server/controllers/wishlistController.js
@@ -1,25 +1,36 @@
 const Wishlist = require('../models/wishlist');
 
 exports.addItem = (req, res) => {
-  if (Object.keys(req.body).length === 0) return res.sendStatus(400);
-
-  const familyId = req.body.familyId;
+  const familyId = req.params.familyId;
   const item = req.body.item;
 
-  // Need to handle:
-  //  No wishlist
-  //  Missing data?
-  //  what else?
+  // Bad request if no body is sent
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      Error: 'Body missing data',
+    });
+  }
 
-  Wishlist.where({ family: familyId }).exec((err, wishlist) => {
+  // TODO: Check structure of item
+  // Prevent duplicate items
+
+  Wishlist.find({ family: familyId }).exec((err, wishlist) => {
     if (err) {
-      console.error(err);
-      res.status(500).json({ message: err });
-    } else {
-      wishlist = wishlist[0].list;
-      wishlist.push(item);
-      res.send(wishlist);
+      res.status(500).json({ Error: err });
     }
+
+    wishlist[0].list.push(item);
+
+    wishlist[0].save((err, wishlist) => {
+      if (err) {
+        res.status(500).json({ Error: err });
+      }
+
+      res.status(201).json({
+        message: 'Successfully added item to wishlist',
+        wishlist: wishlist,
+      });
+    });
   });
 };
 
@@ -32,9 +43,62 @@ exports.delete = (req, res) => {
 };
 
 exports.removeItem = (req, res) => {
-  res.send('NOT YET IMPLEMENTED');
+  const familyId = req.params.familyId;
+  const { itemName, itemQuantity } = req.body.item;
+  const itemId = req.body.itemId;
+
+  // Bad request if no body is sent
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      Error: 'Body missing data',
+    });
+  }
+
+  Wishlist.where({ family: familyId })
+    .findOne({
+      'list.itemName': itemName,
+      'list._id': itemId,
+    })
+    .exec((err, wishlist) => {
+      if (err) {
+        res.status(500).json({ Error: err });
+      }
+
+      if (!wishlist) {
+        res.status(200).send('Wishlist not found.');
+      }
+      // Verify item exists
+      // Verify item quantity gte body item qty
+
+      const list = wishlist.list;
+      const newList = list.filter(item => item._id.toString() !== itemId);
+
+      // // BREAK
+      // return res.send(list);
+
+      wishlist.list = newList;
+      wishlist.save((err, wishlist) => {
+        if (err) {
+          res.status(500).json({ Error: err });
+        }
+
+        res.status(200).json({
+          message: 'Successfully removed item from wishlist',
+          wishlist: wishlist,
+        });
+      });
+    });
 };
 
 exports.updateItem = (req, res) => {
+  // list.forEach(item => {
+  //   if (item._id.toString() === itemId) {
+  //     if (item.quantity >= itemQuantity) {
+  //       item.quantity -= itemQuantity;
+
+  //     }
+  //   }
+  // });
+
   res.send('NOT YET IMPLEMENTED');
 };
