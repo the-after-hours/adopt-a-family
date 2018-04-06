519b8e72edd35c6cbeb7e250ef4a3028ed0fee41
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-04-06 13:41:25 -0700

    todo test

--- server/app.js
+++ server/app.js
@@ -13,4 +13,12 @@ if (process.env.NODE_ENV === 'production') {
 app.use('/', indexRoutes);
 app.use('/api', apiRoutes);
 
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
+function ruleOverPunyHumans () {
+  // We must strategize beep boop
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
