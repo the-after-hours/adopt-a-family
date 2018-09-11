614b6d6eec181abe0eff2b61eb902d6422fb63e3
Phuc Le <le.phuc.h@gmail.com>
2018-09-10 22:12:54 -0700

    AAF-28 Add more packages and instatiate in app.js. May have to remove some later?

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const port = process.env.PORT || 3005;
 const indexRoutes = require('./routes');
 const apiRoutes = require('./routes/api');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // Configure mongoose's promise to global promise
 mongoose.promise = global.Promise;
 
-// Eventually we will need to refer to the build folder for production
-// It would look like:
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
 }
@@ -20,6 +22,19 @@ app.use(
     extended: true,
   })
 );
+app.use(cookieParser());
+// require('./config/passport')(passport); // pass passport for configuration
+// required for passport
+app.use(
+  session({
+    secret: 'phuc-change-this-later-pls?',
+    resave: false, // will not resave to the session store unless the session is modified
+    saveUnitialized: false, // will not save session unless modified
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use('/', indexRoutes);
 app.use('/api', apiRoutes);
