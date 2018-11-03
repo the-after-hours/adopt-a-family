1a358dc85c6aa20d896f8f21d0d5baba5cf3dd35
Phuc Le <le.phuc.h@gmail.com>
2018-11-03 08:44:47 +0100

    Update passport config for correct local auth and pass in passport as an arg.

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,44 +1,51 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
+/**
+ * Container function for passport configs
+ * Imported and called in server/app.js and given the param passport
+ * @param passport Passport module
+ */
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
   });
-});
 
-passport.use(
-  'local-signup',
-  new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true, //allows us to pass back the entire request to the callback
-  }),
-  function(req, email, password, done) {
-    const query = User.findOne({ email });
-
-    query.exec((err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (user) {
-        return done(null, false, {
-          message: `There is alread a user with email: ${email}`,
-        });
-      } else {
-        const newUser = new User({
-          local: {
-            email,
-            password,
-          },
-        });
-
-        return done(null, user);
-      }
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
     });
-  }
-);
+  });
+
+  /**
+   * Configures the local auth strategy, ie, the one where users are authenticated via our own DB
+   */
+  passport.use(
+    new LocalStrategy(
+      {
+        // Declare custom usernameField and passwordField labels
+        usernameField: 'email',
+        passwordField: 'password',
+      },
+      // Authentication callback
+      function(email, password, done) {
+        const query = User.findOne({ email });
+
+        query.exec((err, user) => {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, {
+              message: `There is no registered user with email: ${email}`,
+            });
+          }
+          if (!user.isValidPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' });
+          }
+          return done(null, user);
+        });
+      }
+    )
+  );
+};
