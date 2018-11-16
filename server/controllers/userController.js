6a2fca366c55bbd34b393abc9930e786e5dc8b5c
Phuc Le <le.phuc.h@gmail.com>
2018-11-16 00:46:14 -0800

    Extend signUp in userController so it works with User model.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,13 @@
 const User = require('../models/user');
+const Donor = require('../models/donor');
+const Family = require('../models/family');
+const Organizer = require('../models/organizer');
+const Name = require('../models/name');
 const passport = require('passport');
+const ACCOUNT_TYPES = require('../constants/accountTypes');
 
 exports.signUp = (req, res) => {
-  console.log(req);
+  console.log(req.body);
   const {
     firstName,
     middleInitial,
@@ -13,47 +18,76 @@ exports.signUp = (req, res) => {
     password,
   } = req.body;
 
-  const queryForExistingUser = User.findOne({ email });
+  const queryForExistingUser = User.findOne({ email: email });
 
-  queryForExistingUser.exec((error, user) => {
-    if (error) {
-      return res.status(500).json({
-        error: error,
-        message: 'Error while trying to find pre-existing user.',
-      });
-    } else if (user) {
-      res.json({
-        message: `Sorry, there is already a user with the email ${email}`,
-      });
-    } else {
-      // Ok to create the new user
-      const newUser = new User({
-        local: {
-          firstName,
-          middleInitial,
-          lastName,
-          address,
-          accountType,
-          email,
-          password,
-        },
-      });
+  queryForExistingUser
+    .exec((error, user) => {
+      if (error) {
+        return res.status(500).json({
+          error: error,
+          message: 'Error while trying to find pre-existing user.',
+        });
+      } else if (user) {
+        res.json({
+          message: `Sorry, there is already a user with the email ${email}`,
+        });
+      } else {
+        // Ok to create the new user
 
-      newUser.save((error, savedUser) => {
-        if (error) {
-          return res.status(500).json({
-            error,
-            message: 'Error while attempting to save new user.',
-          });
+        const nameSchema = new Name({
+          first: firstName,
+          middle: middleInitial,
+          last: lastName,
+        });
+
+        let accountTypeSchema;
+        switch (accountType) {
+          case ACCOUNT_TYPES.ORGANIZER:
+            accountTypeSchema = new Organizer({
+              name: nameSchema,
+              organization,
+            });
+          case ACCOUNT_TYPES.DONOR:
+            accountTypeSchema = new Donor({
+              name: nameSchema,
+              budget,
+            });
+          case ACCOUNT_TYPES.FAMILY:
+          default:
+            // Default to family account since it has the least power
+            accountTypeSchema = new Family({
+              name: nameSchema,
+            });
         }
 
-        res.status(200).json({
-          message: 'Successfully created new user.',
-          savedUser,
+        const newUser = new User({
+          local: {
+            name: nameSchema,
+            address,
+            accountType: accountTypeSchema,
+            email,
+            password,
+          },
         });
-      });
-    }
-  });
+
+        newUser.save((error, savedUser) => {
+          if (error) {
+            return res.status(500).json({
+              error,
+              message: 'Error while attempting to save new user.',
+            });
+          }
+
+          res.status(200).json({
+            message: 'Successfully created new user.',
+            savedUser,
+          });
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 exports.login = (req, res, next) => {
