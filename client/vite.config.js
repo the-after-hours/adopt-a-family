bbeb401dd0a74d93c46696f8fa4107ea88ebde47
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-28 22:06:05 -0700

    chore: move client files to client directory, add corresponding server configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    port: process.env.UI_PORT || 3000,
+  },
+  preview: {
+    port: 3000,
+  },
 });
